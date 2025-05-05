77a22b05d95b0cc51adec13fbf2a747f5919d1dd
isaias <isaldmart@gmail.com>
2025-05-05 11:40:02 -0600

    Creado archivo alumno.py y web.py; Escrito UI básica en web.py y esqueleto base de clase Alumno y AdminAlumno en alumno.py.

---
+++ mini-proyecto2/alumno.py
@@ -0,0 +1,11 @@
+from dataclasses import dataclass
+
+@dataclass
+class Alumno:
+    codigo: int
+    semestre: int
+    promedio: float
+
+# Parte 1 y Parte 2.
+class AdministradorAlumnos:
+    pass
